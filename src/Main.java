53deeae3023baadb8cf4d5c6d5da760697ff3f4b
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-03 22:16:10 -0400

    JAVA-011 - Agregar menu con while

--- src/Main.java
+++ src/Main.java
@@ -6,35 +6,60 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        int opcion = -1;
 
-        System.out.print("Ingrese nombre: ");
-        String nombre = scanner.nextLine();
+        while (opcion != 0) {
 
-        System.out.print("Ingrese carrera: ");
-        String carrera = scanner.nextLine();
+            System.out.println("=== SISTEMA DUOC ===");
+            System.out.println("1. Mostrar estado");
+            System.out.println("2. Procesar operación");
+            System.out.println("0. Salir");
 
-        System.out.print("Ingrese edad: ");
-        int edad = scanner.nextInt();
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
 
-
-        Estudiante estudiante = new Estudiante(nombre, carrera, edad);
-
-        estudiante.mostrarInformacion();
-        if (edad < 18) {
-            System.out.println("Estudiante menor de edad.");
-        } else if (edad < 25) {
-            System.out.println("Estudiante joven.");
-        } else {
-            System.out.println("Estudiante adulto.");
-
-
-            for (int i = 1; i <= 5; i++) {
-                System.out.println("Procesando estudiante " + i);
+            if (opcion == 1) {
+                System.out.println("Sistema operativo.");
+            } else if (opcion == 2) {
+                System.out.println("Procesando operación...");
+            } else if (opcion == 0) {
+                System.out.println("Cerrando sistema...");
             }
-        }
-    }
 
+
+            System.out.print("Ingrese nombre: ");
+            String nombre = scanner.nextLine();
+
+            System.out.print("Ingrese carrera: ");
+            String carrera = scanner.nextLine();
+
+            System.out.print("Ingrese edad: ");
+            int edad = scanner.nextInt();
+
+
+            Estudiante estudiante = new Estudiante(nombre, carrera, edad);
+
+            estudiante.mostrarInformacion();
+            if (edad < 18) {
+                System.out.println("Estudiante menor de edad.");
+            } else if (edad < 25) {
+                System.out.println("Estudiante joven.");
+            } else {
+                System.out.println("Estudiante adulto.");
+
+
+                for (int i = 1; i <= 5; i++) {
+                    System.out.println("Procesando estudiante " + i);
+                }
+            }
+
+        }
+
+    }
 }
+
+
+
 
 
 
